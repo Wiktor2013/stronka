29754db6cef4f3c6a86287694f2df47a4b88154d
sanczo <wiktor_m@yahoo.com>
2021-09-15 17:58:29 +0200

    cos prostszego

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dupa'
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return 'Hello world'
+    # if request.method == "POST":
+    #     dane = dict(request.form)
+    #     print(dane)
+    #     # return f"Thanks, {request.form.get('name')}"
+    #     with open("complaints.txt", "a") as f:
+    #         f.write(dane["name"] + ";" + dane["email"] + ";" + dane["complain"] + "\n")
+    #     return render_template("home.html", message=f"Thanks, {dane['name']}")
+
+    return render_template("home.html", message="")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    return render_template('login.html', boolean=True)
+
+
+
+
 
