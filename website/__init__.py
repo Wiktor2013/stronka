1c9a85205d75bca3f57d81bb7a66890e99e20fe3
sanczo <wiktor_m@yahoo.com>
2021-09-21 22:09:23 +0200

    walka z flaskiem

--- website/__init__.py
+++ website/__init__.py
@@ -5,20 +5,10 @@ app.config['SECRET_KEY'] = 'dupa'
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    # if request.method == "POST":
-    #     dane = dict(request.form)
-    #     print(dane)
-    #     # return f"Thanks, {request.form.get('name')}"
-    #     with open("complaints.txt", "a") as f:
-    #         f.write(dane["name"] + ";" + dane["email"] + ";" + dane["complain"] + "\n")
-    #     return render_template("home.html", message=f"Thanks, {dane['name']}")
+    user = {'username': 'Wiktor'}
+    return render_template('home.html', title='Home', user=user)
 
-    return render_template("home.html", message="")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    data = request.form
-    return render_template('login.html', boolean=True)
 
 
 
